0b1c048489f33efa09557e2f0686e5cc06fb753d
Hiroaki Nakamura <func.nakamura@1pac.jp>
2014-02-22 11:56:04 +0900

    Use anonymous functions for event handlers to reduce the code size.

--- jquery.japanese-input-change.js
+++ jquery.japanese-input-change.js
@@ -21,59 +21,6 @@
           clearTimeout(timer);
           timer = undefined;
         }
-      },
-      onFocus = function(e) {
-        oldVal = $(e.target).val();
-        readyToSetTimer = true;
-      },
-      onBlur = function(e) {
-        clearTimer();
-        callHandler(e);
-      },
-      onKeyup = function(e) {
-        clearTimer();
-
-        // When Enter is pressed, IME commits text.
-        if (e.which == 13 || isFirefox) {
-          readyToSetTimer = true;
-        }
-
-        // Set timer only when IME does not have uncommitted text.
-        if (readyToSetTimer) {
-          timer = setTimeout(function() {
-            // Check readyToSetTimer again here for the scenario below.
-            // 1. The user change the text.
-            // 2. The timer is set.
-            // 3. The user presses keys and IME has some uncommitted text.
-            //    before timer fires.
-            if (readyToSetTimer) {
-              callHandler(e);
-            }
-          }, delay);
-        }
-      },
-      onKeydown = function(e) {
-        if (isFirefox) {
-          // Firefox fires keydown for the first key, does not fire
-          // keydown nor keyup event during IME has uncommitted text, 
-          // fires keyup when IME commits or deletes all uncommitted text.
-          readyToSetTimer = false;
-        } else {
-          // IE, Chrome and Safari fires events with e.which = 229 for
-          // every keydown during IME has uncommitted text.
-          // Note:
-          // For IE, Chrome and Safari, I cannot detect the moment when
-          // you delete all uncommitted text with pressing ESC or Backspace
-          // appropriate times, so readyToSetTimer remains false at the moment.
-          //
-          // However, it is not a problem. Because the text becomes same
-          // as oldVal at the moment, we does not invoke handler anyway.
-          //
-          // Next time key is pressed and if it causes text to change,
-          // keydown with e.which != 229 occurs, readyToSetTimer becomes
-          // true and handler will be invoked.
-          readyToSetTimer = (e.which != 229);
-        }
       };
 
     if (handler === undefined) {
@@ -81,9 +28,59 @@
       delay = selector;
       selector = null;
     }
-    return this.on('focus', selector, onFocus)
-      .on('blur', selector, onBlur)
-      .on('keyup', selector, onKeyup)
-      .on('keydown', selector, onKeydown);
+
+    return this.on('focus', selector, function(e) {
+      oldVal = $(e.target).val();
+      readyToSetTimer = true;
+    })
+    .on('blur', selector, function(e) {
+      clearTimer();
+      callHandler(e);
+    })
+    .on('keyup', selector, function(e) {
+      clearTimer();
+
+      // When Enter is pressed, IME commits text.
+      if (e.which == 13 || isFirefox) {
+        readyToSetTimer = true;
+      }
+
+      // Set timer only when IME does not have uncommitted text.
+      if (readyToSetTimer) {
+        timer = setTimeout(function() {
+          // Check readyToSetTimer again here for the scenario below.
+          // 1. The user change the text.
+          // 2. The timer is set.
+          // 3. The user presses keys and IME has some uncommitted text.
+          //    before timer fires.
+          if (readyToSetTimer) {
+            callHandler(e);
+          }
+        }, delay);
+      }
+    })
+    .on('keydown', selector, function(e) {
+      if (isFirefox) {
+        // Firefox fires keydown for the first key, does not fire
+        // keydown nor keyup event during IME has uncommitted text, 
+        // fires keyup when IME commits or deletes all uncommitted text.
+        readyToSetTimer = false;
+      } else {
+        // IE, Chrome and Safari fires events with e.which = 229 for
+        // every keydown during IME has uncommitted text.
+        // Note:
+        // For IE, Chrome and Safari, I cannot detect the moment when
+        // you delete all uncommitted text with pressing ESC or Backspace
+        // appropriate times, so readyToSetTimer remains false at the moment.
+        //
+        // However, it is not a problem. Because the text becomes same
+        // as oldVal at the moment, we does not invoke handler anyway.
+        //
+        // Next time key is pressed and if it causes text to change,
+        // keydown with e.which != 229 occurs, readyToSetTimer becomes
+        // true and handler will be invoked.
+        readyToSetTimer = (e.which != 229);
+      }
+    });
   };
 }(jQuery));
